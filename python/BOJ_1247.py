7e10aaf98babcf8288d41998bf7149c0b04ca319
hyesungoh <haesungoh414@gmail.com>
2020-08-22 17:20:35 +0900

    BOJ 1247

--- python/BOJ_1247.py
+++ python/BOJ_1247.py
@@ -1,6 +1,15 @@
-s = 0
-for _ in range(int(input())):
-    s += int(input())
-if s == 0: print(0)
-else:
-    print('+' if s > 0 else '-')
+# s = 0
+# for _ in range(int(input())):
+#     s += int(input())
+# if s == 0: print(0)
+# else:
+#     print('+' if s > 0 else '-')
+
+
+import sys
+ip = sys.stdin.readline
+for i in range(3):
+    s = 0
+    for _ in range(int(ip())):
+        s += int(ip())
+    print(('0' if s == 0 else '-' if s < 0 else '+'))
