c93015583f0287f59446ee771cd7dceb612ec75c
hyesungoh <haesungoh414@gmail.com>
2020-12-14 22:54:14 +0900

    BOJ 1699

---
+++ python/BOJ_1699.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [i for i in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+        dp[i] = min(dp[i], dp[i-j*j]+1)
+
+print(dp[n])
