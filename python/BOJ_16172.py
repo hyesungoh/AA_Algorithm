2c2cf66466956f48a3e6060dd44adbd4579bf71f
hyesungoh <haesungoh414@gmail.com>
2021-01-20 20:22:59 +0900

    Update BOJ 16172

--- python/BOJ_16172.py
+++ python/BOJ_16172.py
@@ -1,12 +1,16 @@
+# s = input()
+# k = input()
+#
+# no_num_s = []
+# for i in s:
+#     if not i.isdigit():
+#         no_num_s.append(i)
+# no_num_s = "".join(no_num_s)
+#
+# print(1 if k in no_num_s else 0)
+
 s = input()
 k = input()
-
-no_num_s = []
-
-for i in s:
-    if not i.isdigit():
-        no_num_s.append(i)
-
-no_num_s = "".join(no_num_s)
-
-print(1 if k in no_num_s else 0)
+for i in "0123456789":
+    s = s.replace(i, "")
+print(1 if k in s else 0)
