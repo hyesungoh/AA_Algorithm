2e46e574f6c8905b06c08eb77e1d877b42c688e6
hyesungoh <haesungoh414@gmail.com>
2021-01-21 16:52:31 +0900

    Update BOJ 2798

--- python/BOJ_2798.py
+++ python/BOJ_2798.py
@@ -1,9 +1,22 @@
+# n, m = map(int, input().split())
+# l = list(map(int, input().split()))
+# ans = 0
+# for i in range(n-2):
+#     for j in range(i+1, n-1):
+#         for k in range(j+1, n):
+#             if l[i] + l[j] + l[k] <= m:
+#                 ans = max(ans, l[i] + l[j] + l[k])
+# print(ans)
+
 n, m = map(int, input().split())
-l = list(map(int, input().split()))
-ans = 0
+l = sorted(map(int, input().split()), reverse=True)
+
+s = set()
 for i in range(n-2):
     for j in range(i+1, n-1):
         for k in range(j+1, n):
-            if l[i] + l[j] + l[k] <= m:
-                ans = max(ans, l[i] + l[j] + l[k])
-print(ans)
+            t = l[i] + l[j] + l[k]
+            if t <= m:
+                s.add(t)
+                break
+print(max(s))
