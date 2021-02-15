dd2afe9d66f00595dbd7ba9b8bfed04798beaf77
hyesungoh <haesungoh414@gmail.com>
2021-02-15 20:57:51 +0900

    BOJ 2225

---
+++ python/BOJ_2225.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+dp = [0 for _ in range(n+1)]
+dp[0] = 1
+
+for i in range(1, k+1):
+    for j in range(1, n+1):
+        dp[j] = (dp[j-1] + dp[j]) % 1000000000
+
+print(dp[n])
