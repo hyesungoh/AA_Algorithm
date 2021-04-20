6c315767b31764aa4d7cacf545aeddf2547a07c6
hyesungoh <haesungoh414@gmail.com>
2021-04-21 00:42:59 +0900

    BOJ 11375

---
+++ python/BOJ_11375.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(start):
+    if visit[start] == 1: return 0
+    visit[start] = 1
+
+    for i in S[start]:
+        if E[i] == 0 or dfs(E[i]):
+            E[i] = start
+            return 1
+    return 0
+
+N, M = map(int, input().split())
+S = [[] for _ in range(N+1)]
+E = [0 for _ in range(M+1)]
+ans = 0
+
+for i in range(1, N+1):
+    t = list(map(int, input().split()))
+    S[i].extend(t[1:])
+
+for i in range(1, N+1):
+    visit = [0 for _ in range(N+1)]
+    if dfs(i): ans += 1
+
+print(ans)
