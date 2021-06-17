3ca325a5bb077a4d38df4758fe04aaea2d74c25a
hyesung oh <haesungoh414@gmail.com>
2021-06-17 19:34:25 +0900

    BOJ 4153

---
+++ python/BOJ_4153.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    x, y, z = sorted(map(int, input().split()))
+    if x == y == z == 0: break
+    if x ** 2 + y ** 2 == z ** 2: print("right")
+    else: print("wrong")
